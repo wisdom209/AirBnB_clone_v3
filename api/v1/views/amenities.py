3d2b3969f10669e686fe70e4919fff0f8adffa39
TessyJames28 <tessyjames28@gmail.com>
2023-04-28 13:02:59 +0100

    updated amenities http methods

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -16,6 +16,7 @@ from api.v1.views import app_views
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+
 @app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
 def amenity_obj():
     """function that retrieves and create amenities"""
@@ -26,3 +27,48 @@ def amenity_obj():
             amenitiesList.append(val.to_dict())
         return amenitiesList
 
+    if request.method == 'POST':
+        try:
+            body = request.get_json()
+            if not body:
+                abort(400, "Not a JSON")
+            if body.get("name") is None:
+                abort(400, "Missing name")
+            newObj = Amenity(**body)
+            models.storage.new(newObj)
+            models.storage.save()
+            return newObj.to_dict()
+        except Exception as e:
+            abort(400)
+
+
+@app_views.route('/amenities/<amenities_id>',
+                 methods=['GET', 'PUT', 'DELETE'],
+                 strict_slashes=False)
+def amenity_http_methods(amenities_id):
+    amenitiesNeeded = models.storage.get(classes["Amenity"], amenities_id)
+    if request.method == 'GET':
+        if amenitiesNeeded:
+            return amenitiesNeeded.to_dict()
+        abort(404)
+    if request.method == 'DELETE':
+        if amenitiesNeeded:
+            models.storage.delete(amenitiesNeeded)
+            models.storage.save()
+            return {}
+        abort(404)
+    if request.method == 'PUT':
+        try:
+            body = request.get_json()
+            amenObj = models.storage.get(classes["Amenity"], amenities_id)
+            if not amenObj:
+                abort(404)
+            if not body:
+                abort(404)
+            for key, val in body.items():
+                if key not in ["id", "created_at", "updated_at"]:
+                    setattr(amenObj, key, val)
+            models.storage.save()
+            return amenObj.to_dict()
+        except Exception as e:
+            abort(404)
