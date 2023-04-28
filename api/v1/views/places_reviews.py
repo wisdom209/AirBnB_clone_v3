2cad463bdd81935ec8aec461e2d2f35b1a0678dc
TessyJames28 <tessyjames28@gmail.com>
2023-04-28 20:46:10 +0100

    updated reviews_places methods

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -23,14 +23,32 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 def work_on_all_reviews_by_place_id(place_id):
     """retrieve the state's cities"""
     if request.method == 'GET':
-        place_obj = models.storage.get(classes['Place'], place_id)
-        if not place_obj:
+        all_places = models.storage.get(Place, place_id)
+        if not all_places:
             abort(404)
-        place_list = []
-        all_places = models.storage.all(classes["Place"])
-        for value in all_places.values():
-            place_id.append(value.to_dict())
-        return place_list
+        for reviews in all_places.reviews:
+            return reviews.to_dict()
+
+    if request.method == 'POST':
+        placeObj = models.storage.get(Place, place_id)
+        if not placeObj:
+            abort(404)
+        try:
+            body = request.get_json()
+        except Exception as e:
+            abort(400, "Not a JSON")
+        if body.get("user_id") is None:
+            abort(400, "MIssing user_id")
+        userObj = models.storage.get(User, body["user_id"])
+        if not userObj:
+            abort(404)
+        if "text" not in body:
+            abort(400, "Missing text")
+        body["place_id"] = place_id
+        newObj = Review(**body)
+        models.storage.new(newObj)
+        models.storage.save()
+        return newObj.to_dict(), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET', 'POST', 'DELETE'],
@@ -44,3 +62,21 @@ def work_on_a_review(review_id):
         models.storage.delete(review_obj)
         models.storage.save()
         return {}
+    if request.method == 'GET':
+        review = models.storage.get(Review, review_id)
+        if review:
+            return review.to_dict()
+        abort(404)
+    if request.method == 'PUT':
+        reviewObj = models.storage.get(Review, review_id)
+        if not reviewObj:
+            abort(404)
+        try:
+            body = request.get_json()
+        except Exception as e:
+            abort(400, "Not a JSON")
+        for key, val in body.items():
+            if key not in ["id", "place_id", "created_at", "updated_at"]:
+                setattr(review, key, val)
+        models.storage.save()
+        return reviewObj.to_dict()
