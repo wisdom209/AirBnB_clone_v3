2bbdd52363a8d6d6abbe23d4b8d4288fa749919e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-28 11:53:49 +0100

    update cities api

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Handle state objects"""
 from ast import mod
+import re
 from models.user import User
 from models.state import State
 from models.review import Review
@@ -17,7 +18,8 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'],
+                 strict_slashes=False)
 def get_all_cities_by_stateId(state_id):
     """retrieve the state's cities"""
     if request.method == 'GET':
@@ -30,15 +32,25 @@ def get_all_cities_by_stateId(state_id):
                 city_list.append(value.to_dict())
         return city_list
     if request.method == 'POST':
+        state_obj = models.storage.get(classes['State'], state_id)
+        if not state_obj:
+            abort(404)
         body = request.get_json()
         if not body:
-            abort(404, "Not a JSON")
-        
+            abort(400, "Not a JSON")
+        if body.get("name") is None:
+            abort(400, "Missing name")
+        new_city = City(**body)
+        setattr(new_city, 'state_id', state_id)
+        models.storage.new(new_city)
+        models.storage.save()
+        return make_response(jsonify(new_city.to_dict()), 201)
 
 
-@ app_views.route('/cities/<city_id>', methods=['GET', 'DELETE'], strict_slashes=False)
-def get_a_city(city_id):
-    """get a city by id"""
+@ app_views.route('/cities/<city_id>', methods=['GET', 'DELETE'],
+                  strict_slashes=False)
+def work_on_a_city(city_id):
+    """http methods on a city by id"""
     if request.method == 'GET':
         city = models.storage.get(classes["City"], city_id)
         if not city:
@@ -51,3 +63,15 @@ def get_a_city(city_id):
         models.storage.delete(city)
         models.storage.save()
         return {}
+    if request.method == 'PUT':
+        city = models.storage.get(classes["City"], city_id)
+        if not city:
+            abort(404)
+        body = request.get_json()
+        if not body:
+            abort(400, "Not a JSON")
+        for k, v in body.items():
+            if k not in ["id", "state_id", "created_at", "updated_at"]:
+                setattr(city, k, v)
+        models.storage.save()
+        return make_response(jsonify(city.to_dict()), 200)
